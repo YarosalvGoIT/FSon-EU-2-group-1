6f3f8d9f45ae2115cb829db3831bb1b3478dd7e0
Yaroslav-Zozulya <113194791+YarosalvGoIT@users.noreply.github.com>
2023-01-20 17:50:42 +0300

    add tasks

---
+++ js/task-2.js
@@ -0,0 +1,7 @@
+/**
+ *?      Напишите программу, которая получит от пользователя
+ *?      число (количество минут) и выведет в консоль
+ *?      строку в формате часов и минут
+ *?      https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
+ *?      70 === 01:10
+ */
